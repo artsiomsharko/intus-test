8aa90e360227b74163552530779125cdd5ca67e0
Artsiom Sharko <artsiom.sharko@innowise-group.com>
2022-06-09 20:45:48 +0300

    init application

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <BaseHeader />
+  <RouterView />
+</template>
+
+<script setup>
+import BaseHeader from "./components/BaseHeader.vue";
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="w-screen shadow text-center text-[32px] py-4">
+    User managment
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
